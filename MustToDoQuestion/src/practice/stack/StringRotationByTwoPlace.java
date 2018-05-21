61b1176766c159434daa1284fc40e65fc8094f8e
Mahendra Prajapati <pr-dhruv@users.noreply.github.com>
2018-05-21 19:14:10 +0530

    ..

--- MustToDoQuestion/src/practice/stack/StringRotationByTwoPlace.java
+++ MustToDoQuestion/src/practice/stack/StringRotationByTwoPlace.java
@@ -12,12 +12,23 @@ public class StringRotationByTwoPlace {
 		
 		String[] data = new String[2 * n];
 		
-		for( int i = 0; i < 2 * n; i++) {
+		for( int i = 0; i < 2 * n; i++)
 			data[i] = sc.next();
-			data[i+1] = sc.next();
+		
+		for (int  i = 0; i < 2 * n; i+=2) {
+			String s1 = data[i];
+			String s2 = data[i+1];
+			
+			String s3 = s1.substring(2) + s1.substring(0, 2);
+			String s4 = s1.substring(s1.length() - 2) + s1.substring(0,s1.length() - 2);			
+			
+			if(s2.equals(s3))
+				System.out.println("1");
+			else if(s2.equals(s4))
+				System.out.println("1");
+			else
+				System.out.println("0");
 		}
-		
-		
 		
 	}
 
---
+++ MustToDoQuestion/test/SubString.java
@@ -0,0 +1,14 @@
+
+public class SubString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "amazon";
+		System.out.println(s);
+		System.out.println(s.substring(2)+s.substring(0,2));
+		System.out.println(s.substring(s.length() - 2)+s.substring(0,s.length() - 2));
+		
+	}
+
+}
